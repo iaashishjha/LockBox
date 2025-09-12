4e4e989fa4fa56376f352c2f3a5d986608a2da0d
Aashish Jha <er.aashishjha@gmail.com>
2025-09-12 16:29:08 +0530

    success

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,7 @@ function App() {
     <>
         <Navbar /> 
 
-        {/* Collection of modern, background snippets :
-            Ready-to-use, simply copy and paste into your next project. All snippets crafted with Tailwind CSS and Vanilla CSS for easy integration.
-            Website: https://bg.ibelick.com/ */}
+        
 
         <div className="bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
 
